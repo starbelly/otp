b112b1173832999848ab53336ebd4e2ac404f193
Bryan Paxton <starbelly@pobox.com>
2020-09-13 18:51:51 -0500

    wip

---
+++ lib/crypto/eq_const_time.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#erlperf 'crypto:equal_const_time(<<"onexxxxxxxxxxxxxxxxxxxxxxxxxxxxx">>, <<"twoxxxxxxxxxxxxxxxxxxxxxxxxxxxxx">>).' 'crypto:old_equal_const_time(<<"onexxxxxxxxxxxxxxxxxxxxxxxxxxxxx">>, <<"twoxxxxxxxxxxxxxxxxxxxxxxxxxxxxx">>).' --samples 100 --warmup 1
+
+erlperf 'crypto:equal_const_time(<<"onexxxxxxxxxxxxxxxxxxxxxxxxxxxxx">>, <<"twoxxxxxxxxxxxxxxxxxxxxxxxxxxxxx">>).' 'crypto:secure_compare(<<"onexxxxxxxxxxxxxxxxxxxxxxxxxxxxx">>, <<"twoxxxxxxxxxxxxxxxxxxxxxxxxxxxxx">>).' --samples 100 --warmup 1
+
+
+#erlperf 'crypto:equal_const_time("onexxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "twoxxxxxxxxxxxxxxxxxxxxxxxxxxxxx").' 'crypto:old_equal_const_time("onexxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "twoxxxxxxxxxxxxxxxxxxxxxxxxxxxxx").' --samples 100 --warmup 1
