b96b1671a8e4da6384152d33e131f0b623febb2d
Bryan Paxton <starbelly@pobox.com>
2020-09-14 20:49:47 -0500

    wip

--- lib/crypto/src/crypto.erl
+++ lib/crypto/src/crypto.erl
@@ -805,8 +805,7 @@ old_equal_const_time([], [], Truth) ->
 old_equal_const_time(_, _, _) ->
     false.
 
-secure_compare(L, R)
-    when is_binary(L) andalso is_binary(R) ->
+secure_compare(L, R) ->
     case byte_size(L) == byte_size(R) of
         false ->
             false;
